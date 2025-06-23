f841699e19495140259414d791bafe4bb1db9091
Greg Lenaz <greg.lenaz@tetratech.com>
2025-06-22 20:27:37 -0400

    Fix mismatch

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,6 +1,6 @@
 declare module '@capacitor/core' {
   interface PluginRegistry {
-    ZebraCapacitorPlugin: ZebraCapacitorPluginInterface;
+    ZebraCapacitor: ZebraCapacitorPluginInterface;
   }
 }
 
